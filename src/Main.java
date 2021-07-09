70c93f4ceb6e20c91f4c1ec3fcd99bedd09cc5b9
unknown <sauvole75@gmail.com>
2021-07-09 10:18:43 +0700

    add

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+         Student student = new Student();
+         student.setClass("C0521G1");
+         student.setName("Nguyen than");
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private String name = "john";
+    private String Class = "C02";
+    Student(){
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClass(String aClass) {
+        Class = aClass;
+    }
+}
